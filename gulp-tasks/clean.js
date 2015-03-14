b9a80b408ff579b74da9ffd32cc5a6c26d302f6d
Jack Tzu-Chieh Huang <u9520107@gmail.com>
2015-03-14 15:51:02 +0800

    resolve conflict

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import gulp from 'gulp';
 import rm from 'greasebox/rm';
 import path from 'path';
@@ -16,26 +15,4 @@ gulp.task('clean:build', (cb) => {
   rm(path.resolve(__dirname, `../${config.paths.build}`))
     .then(cb)
     .catch(cb);
-=======
-
-import gulp from 'gulp';
-import co from 'co';
-import path from 'path';
-import plumber from 'gulp-plumber';
-import rm from 'greasebox/rm';
-
-import { paths } from './config';
-
-
-gulp.task('clean', (cb) => {
-  rm(paths.build)
-  .then(cb)
-  .catch(cb);
-});
-
-gulp.task('clean:bundle', (cb) => {
-  rm(paths.bundle)
-  .then(cb)
-  .catch(cb);
->>>>>>> 42638c287be161d6e68d0453e5dff3d589819622
 });
--- gulp-tasks/config.js
+++ gulp-tasks/config.js
@@ -1,16 +1,10 @@
 import gulp from 'gulp';
 
-<<<<<<< HEAD
 export const paths = {
-=======
-
-export const paths =  {
->>>>>>> 42638c287be161d6e68d0453e5dff3d589819622
   source: 'source',
   build: 'build',
   bundle: 'bundle'
 };
-<<<<<<< HEAD
 export const bundles = {
 
 };
@@ -25,24 +19,3 @@ gulp.task('config', () => {
     bundles
   }, null, 2));
 });
-=======
-
-export const babelOptions = {
-    optional: ['runtime']
-};
-
-
-export const bundles = {
-  'base': [
-    'react',
-    'co',
-    'meepworks/client-app-driver',
-    'meepworks/uuid',
-    'meepworks/tmpl',
-    'meepworks/styles',
-    'greasebox'
-  ]
-};
-
-gulp.task('config', ()=>{});
->>>>>>> 42638c287be161d6e68d0453e5dff3d589819622
